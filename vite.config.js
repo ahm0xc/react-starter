f78925108f8f637e851f2fb15bf238d7178d8156
ahm0xc <codingwebsa@gmail.com>
2025-03-21 21:31:01 +0600

    fix: ts alias resolving

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
+  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact(), tsconfigPaths()],
   test: {
     globals: true,
     environment: "jsdom",
